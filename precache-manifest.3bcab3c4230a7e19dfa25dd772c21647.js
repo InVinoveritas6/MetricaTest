ccbd1628309fbc1f8502f45774fd0f3a8031bcd8
Alexandr <Aliaksandr.Lahodzich@shate-m.com>
2023-03-31 16:13:50 +0300

    Updates

---
+++ precache-manifest.3bcab3c4230a7e19dfa25dd772c21647.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "fac9ac34a5f88db38227",
+    "url": "/MetricaTest/static/css/main.73d69792.chunk.css"
+  },
+  {
+    "revision": "fac9ac34a5f88db38227",
+    "url": "/MetricaTest/static/js/main.fac9ac34.chunk.js"
+  },
+  {
+    "revision": "e8e3b76c5bf759905946",
+    "url": "/MetricaTest/static/js/1.e8e3b76c.chunk.js"
+  },
+  {
+    "revision": "ae1f4f8dbe9089239656",
+    "url": "/MetricaTest/static/js/2.ae1f4f8d.chunk.js"
+  },
+  {
+    "revision": "69923f5f915033ac5f89",
+    "url": "/MetricaTest/static/js/runtime~main.69923f5f.js"
+  },
+  {
+    "revision": "17447227e0a57c877428ea36241cbc5e",
+    "url": "/MetricaTest/index.html"
+  }
+];
