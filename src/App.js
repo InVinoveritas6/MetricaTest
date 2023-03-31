495c783dcf789d0ea7eeccb30a91d69deb440161
Alexandr <Aliaksandr.Lahodzich@shate-m.com>
2023-03-31 11:45:16 +0300

    sme changes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,41 @@
-import './App.css';
+import "./App.css";
+import { Routes, Route, Link } from "react-router-dom";
+import Page1 from "./pages/page1";
+import Page2 from "./pages/page2";
+import Page3 from "./pages/page3";
+import Homepage from "./pages/HomePage";
+
+// import logo from "../public/logo192.png"
 
 function App() {
   return (
     <div className="App">
-        <div>Hello world</div>
+      <div>
+        <div className="header">
+          <div className="some-logo">
+            <div className="logo">
+              <Link to="/">Logo</Link>
+            </div>
+          </div>
+          <div className="menu">
+            <div className="page">
+              <Link to="/page1">page1</Link>
+            </div>
+            <div className="page">
+              <Link to="/page2">page2</Link>
+            </div>
+            <div className="page">
+              <Link to="/page3">page3</Link>
+            </div>
+          </div>
+        </div>
+      </div>
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/page1" element={<Page1 />} />
+        <Route path="/page2" element={<Page2 />} />
+        <Route path="/page3" element={<Page3 />} />
+      </Routes>
     </div>
   );
 }
